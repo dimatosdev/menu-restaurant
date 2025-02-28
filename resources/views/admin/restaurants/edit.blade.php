354759ce7b3e1f2406848b49f70bc2de62c56038
dimatosdev <dimatosdev@gmail.com>
2025-02-28 15:26:52 -0300

    Implement CRUD Users

--- resources/views/admin/restaurants/edit.blade.php
+++ resources/views/admin/restaurants/edit.blade.php
@@ -1,37 +1,44 @@
-<h1>Edição de restaurante</h1>
-<hr>
-@if($errors->any())
-    <div>
-        <ul>
-            @foreach($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
-<form action="{{ route('restaurant.update', ['restaurant' => $restaurant->id]) }}" method="post">
-    {{ csrf_field() }}
-    {{ method_field('PUT') }}
-    <p>
-        <label for="name">Nome do Restaurante</label><br>
-        <input type="text" name="name" value="{{ old('name', $restaurant->name) }}">
-        @if($errors->has('name'))
-            <span>{{ $errors->first('name') }}</span>
-        @endif
-    </p>
-    <p>
-        <label for="address">Endereço</label><br>
-        <input type="text" name="address" value="{{ old('address', $restaurant->address) }}">
-        @if($errors->has('address'))
-            <span>{{ $errors->first('address') }}</span>
-        @endif
-    </p>
-    <p>
-        <label for="description">Sobre o restaurante</label><br>
-        <textarea name="description" id="description" rows="10">{{ old('description', $restaurant->description) }}</textarea>
-        @if($errors->has('description'))
-            <span>{{ $errors->first('description') }}</span>
-        @endif
-    </p>
-    <input type="submit" value="Atualizar">
-</form>
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h1>Edição de restaurante</h1>
+    <hr>
+    @if($errors->any())
+        <div>
+            <ul>
+                @foreach($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    <form action="{{ route('restaurant.update', ['restaurant' => $restaurant->id]) }}" method="post">
+        {{ csrf_field() }}
+        {{ method_field('PUT') }}
+        <p>
+            <label for="name">Nome do Restaurante</label><br>
+            <input type="text" name="name" value="{{ old('name', $restaurant->name) }}" class="form-control @if($errors->has('name')) is-invalid @endif">
+            @if($errors->has('name'))
+                <span class="invalid-feedback">{{ $errors->first('name') }}</span>
+            @endif
+        </p>
+        <p>
+            <label for="address">Endereço</label><br>
+            <input type="text" name="address" value="{{ old('address', $restaurant->address) }}" class="form-control @if($errors->has('address')) is-invalid @endif">
+            @if($errors->has('address'))
+                <span class="invalid-feedback">{{ $errors->first('address') }}</span>
+            @endif
+        </p>
+        <p>
+            <label for="description">Sobre o restaurante</label><br>
+            <textarea name="description" id="description" rows="10" class="form-control @if($errors->has('description')) is-invalid @endif">{{ old('description', $restaurant->description) }}</textarea>
+            @if($errors->has('description'))
+                <span class="invalid-feedback">{{ $errors->first('description') }}</span>
+            @endif
+        </p>
+        <input type="submit" value="Atualizar" class="btn btn-success btn-lg">
+    </form>
+</div>
+@endsection
+
--- resources/views/admin/restaurants/index.blade.php
+++ resources/views/admin/restaurants/index.blade.php
@@ -1,30 +1,37 @@
-<h1>Restaurantes</h1>
-<a href="{{route('restaurant.new')}}">Novo</a>
-<table>
-    <thead>
-        <tr>
-            <th>#</th>
-            <th>Nome</th>
-            <th>Criado em</th>
-            <th>Ações</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach($restaurants as $restaurant)
-        <tr>
-            <td>{{ $restaurant->id }}</td>
-            <td>{{ $restaurant->name }}</td>
-            <td>{{ $restaurant->created_at->format('d/m/Y H:i') }}</td>
-            <td>
-                <a href="{{ route('restaurant.edit', ['restaurant' => $restaurant->id]) }}">Editar</a>
-                <form action="{{ route('restaurant.destroy', ['restaurant' => $restaurant->id]) }}" method="post" style="display:inline;" onsubmit="return confirm('Tem certeza que deseja deletar este restaurante?');">
-                    {{ csrf_field() }}
-                    {{ method_field('DELETE') }}
-                    <button type="submit">Deletar</button>
-                </form>
-            </td>
-        </tr>
-        @endforeach
-    </tbody>
-</table>
+@extends('layouts.app')
 
+@section('content')
+<div class="container">
+    <h1>Restaurantes</h1>
+    <a href="{{route('restaurant.new')}}" class="float-right btn btn-primary">Novo</a>
+    <table class="table table-stripped">
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>Nome</th>
+                <th>Criado em</th>
+                <th>Ações</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($restaurants as $restaurant)
+            <tr>
+                <td>{{ $restaurant->id }}</td>
+                <td>{{ $restaurant->name }}</td>
+                <td>{{ $restaurant->created_at->format('d/m/Y H:i') }}</td>
+                <td>
+                    <a href="{{ route('restaurant.edit', ['restaurant' => $restaurant->id]) }}" class="btn btn-primary">Editar</a>
+                    <form action="{{ route('restaurant.destroy', ['restaurant' => $restaurant->id]) }}" method="post" style="display:inline;" onsubmit="return confirm('Tem certeza que deseja deletar este restaurante?');">
+                        {{ csrf_field() }}
+                        {{ method_field('DELETE') }}
+                        <button type="submit" class="btn btn-danger">Deletar</button>
+                    </form>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
+
+
+@endsection
